645dc1e22dfa6ec603a29bf18175af62a8f8bd19
Jacob Albright <jacob.albright23@gmail.com>
2018-07-11 23:03:19 -0500

    now the conditional adding of mousemove event listener upon mousedown is dependant upon having clicked an actual piece rather than a tile whose borders contained the center coordinates of a piece

--- game.js
+++ game.js
@@ -1,32 +1,37 @@
 const game = ( ({canvas}, board) => {  
   canvas.addEventListener('mousedown', handleMousedown)
-  
-
 
   function handleMousedown (e) {
     const allPieces = [...board.pieces.p1Pieces, ...board.pieces.p2Pieces];
-    const clicked = clickedTile(e)[0]; 
-    console.log(clicked);
+    const clicked = whichTileGotClicked(e);     
+    const tileHasPiece = clicked.hasPiece(allPieces);
+    const clickedPiece = whichPieceGotClicked(e);
     
-    const piece = clicked.hasPiece(allPieces)[0];
-    console.log(piece);
-    
-    if (piece) {
+    if (clickedPiece) {
       canvas.addEventListener('mousemove', handleMousemove);
       canvas.addEventListener('mouseup', handleMouseup);
     } 
 
-    function clickedTile(e) {
+    function whichTileGotClicked(e) {
       return board.tiles.tilesArray.filter(tile => {
         return tile.x < e.offsetX
           && tile.x + tile.width > e.offsetX
           && tile.y < e.offsetY
           && tile.y + tile.height > e.offsetY;
-      });
+      })[0];
+    }
+
+    function whichPieceGotClicked (e) {
+      return allPieces.filter(piece => {
+        return piece.x + piece.radius > e.offsetX
+          && piece.x - piece.radius < e.offsetX
+          && piece.y + piece.radius > e.offsetY
+          && piece.y - piece.radius < e.offsetY;
+      })[0];
     }
 
     function handleMousemove(e) {
-      piece.changePosition(e)
+      clickedPiece.changePosition(e)
       board.reRenderPieces()
     }
 
