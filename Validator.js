ab9efabf05eac66e03f3b56d9ea912b2064ac785
Jacob Albright <jacob.albright23@gmail.com>
2018-07-19 22:13:22 -0500

    modified Validator class

--- Validator.js
+++ Validator.js
@@ -1,47 +1,47 @@
 const validator = ( () => {
   class Validator {
+  
+    static possibleMoves (piece, game) {
+      const {history} = game;
+    }
 
-    static nextRowIndex (piece, prevRowIndex) {
-      if (piece.player == 'p1') {
-        if (prevRowIndex + 1 < 7) {
-          return prevRowIndex + 1;
-        } else if (prevRowIndex + 1 == 7) {
-          return 'KING KING KING KING KING';
-        }
-      } else if (prevRowIndex - 1 > 0) {
-        return prevRowIndex - 1;
-      } else if (prevRowIndex - 1 == 0) {
-        return 'KING KING KING KING KING'
-      }
-      
+    static nextRowIndex (piece, clickedRowIndex, nRowsAhead) {
+      if (piece.player == 'p1') { 
+        if (clickedRowIndex + nRowsAhead <= 7) {
+          return clickedRowIndex + nRowsAhead;
+        } 
+      } else if (clickedRowIndex - nRowsAhead >= 0) {
+        return clickedRowIndex - nRowsAhead;
+      } 
     }
 
     static nextColIndex (dir, prevColIndex) {
-      return dir == 'right' ? prevColIndex + 1 : prevColIndex - 1;
+      if (dir == 'right') {
+        if (prevColIndex + 1 <= 7) {
+          return prevColIndex + 1;
+        } 
+      } else if (prevColIndex - 1 >= 0) {
+        return prevColIndex - 1;
+      }
+    }
+
+    static nextRow (piece, prevRowIndex, nRowsAhead, currentBoard) {
+      const nextRowIndex = this.nextRowIndex(piece, prevRowIndex, nRowsAhead);      
+      if (!nextRowIndex) {        
+        return 'End of board';
+      }
+      return currentBoard[nextRowIndex];
     }
 
     static possibleMoves (piece, {history}) {
       const currentBoard = history[history.length - 1];
-      const rowIndex = Number(piece.location[0]);
-      const colIndex = Number(piece.location[1]);
-      const row = currentBoard[rowIndex];
-      const nextRowIndex = this.nextRowIndex(piece, rowIndex);
-      const colRightIndex = this.nextColIndex('right', colIndex);
-      const colLeftIndex = this.nextColIndex('left', colIndex);
-      const nextRow = currentBoard[nextRowIndex];
-      console.log('####################### DIVIDER #########################');
-      
-      console.log(piece);
-      console.log('Row', row);
+      const clickedRowIndex = Number(piece.location[0]);
+      const clickedColIndex = Number(piece.location[1]);
+      const nextRow = this.nextRow(piece, clickedRowIndex, 1, currentBoard);
+      const andTheNextRow = this.nextRow(piece, clickedRowIndex, 2, currentBoard);
       console.log('nextRow', nextRow);
-      console.log('nextRowIndex', nextRowIndex);
-      console.log('colIndex', colIndex);
-      console.log('colLeftIndex', colLeftIndex);
-      console.log('colRightIndex', colRightIndex);
-      
-      
-      
-      return true;
+      console.log('andTheNextRow', andTheNextRow);
+ 
     }
 
     static movedFromTile (pos1, pos2) {
