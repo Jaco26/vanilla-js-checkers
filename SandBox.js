7f62322d7c665aaa0e78446290876949c6ddd94a
Jacob Albright <jacob.albright23@gmail.com>
2018-07-26 21:47:58 -0500

    possible moves to the left are being found

--- SandBox.js
+++ SandBox.js
@@ -1,18 +1,24 @@
 const board = [
   [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 1, 0, 1, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0],
 ];
 
 let rowStart = 0; // start row index
 let colStart = 3; // start column index
 
-
+let forkCounter = 0;
 
 function testArraySearch() {
-  fork(board, rowStart, colStart)
+  let viableMoves = [];
+  
+  fork(board, rowStart, colStart, viableMoves)
+  console.log(viableMoves);
+  
 }
 
 /**
@@ -20,31 +26,71 @@ function testArraySearch() {
  * @param {Number} rowI the row index of the 'active' tile
  * @param {Number} colI the column index of the 'active' tile
  */
-function fork(board, rowI, colI) {
-  if (rowI > board.length) {
+function fork(board, activeRowI, colI, viableMoves) {
+  forkCounter += 1;
+  let loggerObj = {
+    activeRowI,
+    colI,
+    viableMoves
+  }
+  console.log('fork number', forkCounter, loggerObj);
+  
+  if (activeRowI > board.length) {
     return;
   }
-  let nextRow = board[rowI + 1];
-  let colLeft = colI - 1;
-  let colRight = colI + 1;
-  console.log(nextRow);
   
-  if (nextRow[colLeft] == 1) {
+  let forwardRowI = activeRowI + 1;
 
-    
+  let nextRow = board[forwardRowI];
+
+  let colLeft = colI - 1;
+
+  let colRight = colI + 1;  
+
+  if ( nextRow[colLeft] == 1 ) {
+
     // if the column to the left of the active column (and one row ahead) contains the number 1
     // (i.e. is occupied) look one tile ahead in the same direction to see if the next tile is empty
-    let next = searchNext(board, rowI, colLeft, -1)
-    console.log('next row, col LEFT', next);
-    if (next == 1) {
+
+    let potentialMove = searchNext(board, forwardRowI, colLeft, -1)
+        
+    if ( potentialMove == 1 ) {
+
       // the tile cannot be jumped to, a path from the active column toward the left does not exist
+      
+      viableMoves.push(activeRowI.toString() + colI.toString());
+
+      return;
+
     } else {
       // the tile can be jumped to, a path from the active column toward the left does exist
-      // treat "next" as a new 'active tile' and search for possible jumps from it.
+      // treat "potentialMove" as a new 'active tile' and search for possible jumps from it.
+      viableMoves.push(activeRowI.toString() + colI.toString());
+      let nextActiveColI = colLeft - 1;
+      let nextActiveRowI = forwardRowI + 1;
+      fork(board, nextActiveRowI, nextActiveColI, viableMoves);
     }
   }
-  if (row[colRight] == 1) {
+  if (nextRow[colRight] == 1) {
     console.log('next row, col RIGHT', searchNext(arr, rowI, colRight, 1));
+    let potentialMove = searchNext(board, forwardRowI, colRight, 1)
+
+    if (potentialMove == 1) {
+
+      // the tile cannot be jumped to, a path from the active column toward the left does not exist
+
+      viableMoves.push(activeRowI.toString() + colI.toString());
+
+      return;
+
+    } else {
+      // the tile can be jumped to, a path from the active column toward the left does exist
+      // treat "potentialMove" as a new 'active tile' and search for possible jumps from it.
+      viableMoves.push(activeRowI.toString() + colI.toString());
+      let nextActiveColI = colLeft + 1;
+      let nextActiveRowI = forwardRowI + 1;
+      fork(board, nextActiveRowI, nextActiveColI, viableMoves);
+    }
   }
 }
 
@@ -52,10 +98,12 @@ function fork(board, rowI, colI) {
 function searchNext(arr, prevRowI, prevColI, dir) {
   // find the next row in the array by accessing the previous row index + 1
   let thisRow = arr[prevRowI + 1];
+  
   // access a column to the left or right of the previous column index
   let thisCol = thisRow[prevColI + dir];
+  
   // return the contents of the column
-  return thisCol
+  return thisCol;
 }
 
 testArraySearch();
