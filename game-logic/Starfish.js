99fe7c42198370e0548dd851f4d3202b10032954
Jacob Albright <jacob.albright23@gmail.com>
2018-09-22 17:18:23 -0500

    writing method to recurse into object and inject a value at a target namespace at the innermost level

--- game-logic/Starfish.js
+++ game-logic/Starfish.js
@@ -16,20 +16,53 @@ const STARFISH = (() => {
     };
   }
 
+  function injectAtLowestLevelOf(obj, targetPropName) {
+    let thisLevel = obj[targetPropName];
+    if (typeof thisLevel[targetPropName] === 'object') {
+      // if value at obj[targetPropName] is an objec that itself has a prop of the same name...
+      // go down again
+      injectAtLowestLevelOf(thisLevel, targetPropName);
+    } else {
+      // we're at the bottom. set the new prop with the passed-in value
+      console.log(thisLevel);
+      
+      thisLevel[targetPropName] = new Base10MatrixNode(thisLevel.value);
+    }
+  } 
+
+  var test = {
+    h: {
+      h: {
+        h: {
+          h: {
+
+          }
+        }
+      }
+    }
+  }
+
+  injectAtLowestLevelOf(test, 'h', 44);
+  console.log(test);
+  
+  
+
   function starfish(matrix, startXYStr) {
-    let accum = [], startPos = Number(startXYStr);
+    let accum = {}, startPos = Number(startXYStr);
+    let control = 0;
     function tenticle(start) {
+      if (control === 10) return;
       if (start) {
         // if this is the first invokation of `tenticle` (tenticle only gets passed a param the first time)
-        accum.push(new Base10MatrixNode(start));
+        accum = new Base10MatrixNode(start);
+        control += 1
         tenticle();
       } else {
         // If tenticle has been invoked more than once
-        const prevResults = Object.keys(accum).filter(key => key !== 'value');
-        // const thisLevelAccum = [];
-        console.log(accum);
-        let i;
-        
+        const prevResultKeys = Object.keys(accum).filter(key => key !== 'value');
+        prevResultKeys.forEach(key => {
+          injectAtLowestLevelOf(accum, key)          
+        });
         
       }
     }
